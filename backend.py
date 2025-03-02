73cefccacdf0c903e02a129ee284dd0c5f32fb74
Techy_Deven <devendragawade3112@gmail.com>
2025-03-02 08:22:25 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -7,29 +7,20 @@ CORS(app)  # Enable CORS for all routes
 # Initialize a variable to store the health data
 health_data = {}
 
-# Endpoint to send the health data to the frontend
-@app.route('/get-health-data', methods=['GET'])
-def get_health_data():
-    try:
-        # Send the health data as JSON
-        return jsonify(health_data), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
 @app.route('/health-data', methods=['POST'])
 def receive_data():
     data = request.get_json()
     
     # Check if the required keys are in the received JSON data
-    if 'heartRate' not in data or 'SpO2' not in data or 'temperature' not in data:
+    if 'heart_rate' not in data or 'SpO2' not in data or 'temperature' not in data:
         return jsonify({"error": "Missing required health data"}), 400
     
-    heart_rate = data['heartRate']
+    heart_rate = data['heart_rate']
     spO2 = data['SpO2']
     temperature = data['temperature']
     
     # Store the received data into the health_data variable
-    health_data['heartRate'] = heart_rate
+    health_data['heart_rate'] = heart_rate
     health_data['SpO2'] = spO2
     health_data['temperature'] = temperature
     
